3166e7d9dcbd46d4fc9e791c9906b6eb89a3fab6
sedanur <117097148+Sedanuratess@users.noreply.github.com>
2023-10-01 12:22:24 +0300

    Create player.java

---
+++ player.java
@@ -0,0 +1,18 @@
+public class player {
+    private int damage;
+    private  int healthy;
+    private int money;
+    private String name;
+    private String charName;        //char=charecter
+     public Player(String name){
+     this.name=name;
+     
+     }
+     public void selectChar(){
+         
+         System.out.println("Karakter 1 :Samuray \t Hasar:5   \tSağlık:21   \t Para:15");
+         System.out.println("Karakter 2 :Okçu    \t Hasar:7    \tSağlık:18  \t Para:20");
+         System.out.println("Karakter 3:Şovalye \t Hasar:3   \tSağlık:24   \t Para:5");
+         
+     
+     }}
